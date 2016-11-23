aadcdc4e553604f32c0d2ce87a379b5968f7af4b
chenzi <admin@chenzi.net>
2016-11-23 15:56:29 +0800

    Change check the reply Object condition.

--- libgearman-server/plugins/queue/redis/queue.cc
+++ libgearman-server/plugins/queue/redis/queue.cc
@@ -129,17 +129,18 @@ gearmand_error_t Hiredis::initialize()
   if (password.size())
   {
     redisReply *reply = (redisReply*)redisCommand(_redis, "AUTH %s", password.c_str());
-    if(reply == NULL || reply->type == REDIS_REPLY_ERROR) 
-      {
-           freeReplyObject(reply);
+    if(strcasecmp(reply->str,"OK") != 0)
+    {
+        char *error_str = reply->str;
+        freeReplyObject(reply);
            
-           return gearmand_log_gerror(
-           GEARMAN_DEFAULT_LOG_PARAM,
-           GEARMAND_QUEUE_ERROR,
-           "Could not auth with redis server,hires auth reply: %s", _redis->errstr);
-      } 
-      freeReplyObject(reply);
-      gearmand_log_debug(GEARMAN_DEFAULT_LOG_PARAM, "Auth success");
+        return gearmand_log_gerror(
+        GEARMAN_DEFAULT_LOG_PARAM,
+        GEARMAND_QUEUE_ERROR,
+        "Could not auth with redis server,and reply: %s", error_str);
+    } 
+    freeReplyObject(reply);
+    gearmand_log_debug(GEARMAN_DEFAULT_LOG_PARAM, "Auth success");
   }
 
   gearmand_info("Initializing hiredis module");
